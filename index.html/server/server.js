f13e84e7ab7533390db68c51dbfc7430c022d075
abhishekmusunuri <abhishekmusunuri2411@gmail.com>
2024-12-25 16:09:07 +0530

    first commit

---
+++ index.html/server/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.send("Hello, World!");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
